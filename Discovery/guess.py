ce2151d69d4d2ac4cac67edf22088cf6d1275b41
mep2664 <mep2664@rit.edu>
2016-02-07 13:04:09 -0500

    guess.py class structure has been tested and it working, we still need to add more guesses though

--- Discovery/guess.py
+++ Discovery/guess.py
@@ -7,13 +7,30 @@ class guess(object):
     goodGuess = []
 
     def __init__(self):
-        self.guessThis = ["admin","employee","private"]
+        self.guessThis = ["admin","employee","private","dashboard","secure","profile",
+                          "users","user","password","applications","phpinfo"]
 
     def startToGuess(self):
+        boolTest = False
         for g in self.guessThis:
             url_guess = self.base_url + g
-            if requests.get(url_guess)._content is not requests.get(self.bad_url)._content:
-                self.goodGuess.append(self, g)
+            if requests.get(url_guess).content != requests.get(self.bad_url).content:
+                self.goodGuess.append(g)
+            php_guess = self.base_url + g + ".php"
+            if requests.get(php_guess).content != requests.get(self.bad_url).content:
+                self.goodGuess.append(g+".php")
+            jsp_guess = self.base_url + g + ".jsp"
+            if requests.get(jsp_guess).content != requests.get(self.bad_url).content:
+                self.goodGuess.append(g+".jsp")
+            html_guess = self.base_url + g + ".html"
+            if requests.get(html_guess).content != requests.get(self.bad_url).content:
+                self.goodGuess.append(g+".html")
+        print(self.goodGuess)
+
+
+if __name__ == "__main__":
+    newGuess = guess()
+    newGuess.startToGuess()
 
 
 
