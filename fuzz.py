88842bf1d3e083370e0c8e8786da86aafdcb4207
Daniel Santoro <dds2130@rit.edu>
2016-02-08 15:33:06 -0500

    added url paramater to command line parser

--- fuzz.py
+++ fuzz.py
@@ -14,12 +14,14 @@ def main():
 	# discover parser
 	d_parser = subparsers.add_parser('discover', usage=msg('discover'), help='Outputs a comprehensive, human-readable list of all discovered inputs to the system.\nTechniques include both crawling and guessing.')
 	d_parser._optionals.title = 'additional arguments'
+	d_parser.add_argument('url')
 	d_parser.add_argument('--custom-auth', type=str, required=False, metavar='', help='Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa).')
 	d_parser.add_argument('--common-word', type=file, required=True, metavar='', help='REQUIRED Newline-delimited file of common words to be used in page guessing and input guessing.')
 	
 	# test parser
 	t_parser = subparsers.add_parser('test', usage=msg('test'), help='Discovers all inputs, then attempts a list of exploit vectors on those inputs.\nReports potential vulnerabilities.')
 	t_parser._optionals.title = 'additional arguments'
+	t_parser.add_argument('url')
 	#t_parser.register('type' 'bool', str2bool)
 	t_parser.add_argument('--custom-auth', type=str, required=False, metavar='', help='Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa).')
 	t_parser.add_argument('--vectors', type=file, required=True, metavar='', help='REQUIRED Newline-delimited file of common exploits to vulnerabilities.')
