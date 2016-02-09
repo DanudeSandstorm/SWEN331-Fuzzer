efd06c119cfa85490c7288371f251e7a74e4edb6
mep2664 <mep2664@rit.edu>
2016-02-08 19:57:31 -0500

    improved parse_url.py

--- discover/parse_url.py
+++ discover/parse_url.py
@@ -1,14 +1,5 @@
 class parse_url(object):
 
     def parseThis(self, url):
-        input = ""
-        index = url.find('?',0)
-        char = url[index+1]
-        if index == -1:
-            return
-        else:
-            while char != '=':
-                input += char
-                index += 1
-                char = url[index+1]
-            return input
+        return url.partition('?')[-1].rpartition('=')[0]
+
