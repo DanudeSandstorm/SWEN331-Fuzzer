d77b60a19c6aa599c33a46d57a912bc348b38ffc
Daniel Santoro <dds2130@rit.edu>
2016-02-22 23:29:31 -0500

    Crawling works properly

--- discover/crawl.py
+++ discover/crawl.py
@@ -1,52 +1,48 @@
 import mechanize
 
 class Crawl(object):
-    base_url = ""
-    curr_url = ""
-    url_content = ""
-    browser = mechanize.Browser()
+    substring = 'href="'
+    base_url = ''
+    browser = None
     url_list = []
     keeper_list = []
 
     def __init__(self, browser, base_url):
         self.browser = browser
         self.base_url = base_url
-        self.url_content = self.browser.open(self.base_url).read()
+        self.url_list.append(base_url)
 
-
-
-    def findHref(self, url):
-        self.url_content = self.browser.open(self.base_url).read()
+    def find_hrefs(self, url_content):
         indexList = []
-        substring = 'href="/'
         currIndex = 0
         while currIndex != -1:
-            currIndex = self.url_content.find(substring,currIndex+1)
+            currIndex = url_content.find(self.substring, currIndex+1)
             indexList.append(currIndex)
         return indexList
 
-    def findUrls(self, url):
-        if url in self.keeper_list:
-            if len(self.url_list) > 0:
-                newUrl = self.url_list.pop(0)
-                self.findUrls(newUrl)
-            else:
-                return self.keeper_list
-        else:
+    def find_urls(self):
+        if len(self.url_list) <= 0:
+            return self.keeper_list
+
+        url = self.url_list.pop()
+        if url not in self.keeper_list:
             self.keeper_list.append(url)
-            indexList = self.findHref(url)
+
+            url_content = self.browser.open(url).read()
+            indexList = self.find_hrefs(url_content)
+            
             for index in indexList[:-1]:
                 urlExt = ""
-                charIndex = index+7
-                char = self.url_content[charIndex]
+                charIndex = index + len(self.substring)
+                char = url_content[charIndex]
+                
                 while char != '"':
                     urlExt += char
                     charIndex += 1
-                    char = self.url_content[charIndex]
-                self.url_list.append(self.base_url + urlExt)
-            if len(self.url_list) > 0:
-                newUrl = self.url_list.pop(0)
-                self.findUrls(newUrl)
-            else:
-                return self.keeper_list
+                    char = url_content[charIndex]
+                
+                if 'http' not in urlExt:
+                    self.url_list.append(self.base_url + urlExt)
+            
+        return self.find_urls()
 
--- discover/discover.py
+++ discover/discover.py
@@ -1,4 +1,5 @@
 import os
+import mechanize
 from guess import Guess
 from crawl import Crawl
 
@@ -8,15 +9,49 @@ class Discover(object):
         #base_url = args.url
         #common_words = args.common_words
         #temp variables
-        base_url = "http://127.0.0.1/"
+        base_url = "http://127.0.0.1/dvwa/"
         common_words = "\\discover\\common_words.txt"
         path = os.getcwd()
 
-        newGuess = Guess(base_url, path+common_words)
-        newCrawl = Crawl(base_url)
+        #Login
+        browser = mechanize.Browser()
+        browser.open(base_url)
+        browser.select_form(nr=0)
+        browser.form['username'] = 'admin'
+        browser.form['password'] = 'password'
+        browser.submit()
 
-        guess = newGuess.startToGuess()
-        crawl = newCrawl.findUrl(base_url)
-        print guess
-        print crawl
+        # s = requests.session()
+        # r = s.get(base_url, verify = False)
+        # print r.cookies
+        # matchme = 'meta content="(.*)" name="csrf-token" /'
+        # csrf = re.search(matchme, str(r.text))
+        # print csrf
+
+        # payload = {
+        #     'username': 'admin',
+        #     'password': 'password',
+        #     'authenticity_token' : csrf.group(1),
+        #     '_portal_session' : r.cookies["_portal_session"]
+        # }
+
+        # r = s.post(url,data=payload,verify = False)
+        # print r.text
+
+        # payload = {
+        #     'username': 'admin',
+        #     'password': 'pssword',
+        # }
+
+        # with requests.session() as c:
+        #     print c.post(base_url + 'login.php', headers=user_agent, data=payload)
+        #     response = c.get(base_url + 'index.php', headers=user_agent)
+        #     print(response.text)
+
+        #guess = Guess(browser, base_url, path+common_words)
+        crawl = Crawl(browser, base_url)
+
+        #print newGuess.startToGuess()
+        crawled_urls = crawl.find_urls()
+        print crawled_urls
 
