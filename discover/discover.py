c15971c14a7e1ff4919c1953de92fc0d2bef40ef
Daniel Santoro <dds2130@rit.edu>
2016-02-25 01:40:24 -0500

    crawling works

--- discover/discover.py
+++ discover/discover.py
@@ -25,17 +25,17 @@ class Discover(object):
         browser.submit()
 
         crawler = Crawl(browser, base_url)
+        print 'Crawling for Urls...'
         crawled_urls = crawler.crawl()
-        print crawled_urls
+        # print crawled_urls
 
-        
-
-
-        # guesser = Guess(browser, common_words)
-        # guessed_urls = guesser.guess(crawled_urls)
+        guesser = Guess(browser, common_words)
+        print 'Guessing Urls...'
+        guessed_urls = guesser.guess(crawled_urls)
         # print guessed_urls
 
-        # found_urls = crawled_urls + guessed_urls
+        found_urls + list(set(crawled_urls) - set(guessed_urls))
+        print found_urls
 
         #TODO
         # Return map url to its paramaters
--- discover/guess.py
+++ discover/guess.py
@@ -18,12 +18,15 @@ class Guess(object):
 
     def guess(self, urls):
         goodGuess = []
-        for g in self.guesses:
-            for ext in self.extentions:
-                url_guess = self.base_url + g + ext
-                try:
-                    self.browser.open(url_guess).read()
-                    goodGuess.append(url_guess)
-                except mechanize.HTTPError as e:
-                    pass
+        for base_url in urls:
+            if not base_url.endswith('/'):
+                continue      
+            for guess in self.guesses:
+                for ext in self.extentions:
+                    url_guess = base_url + guess + ext
+                    try:
+                        self.browser.open(url_guess).read()
+                        goodGuess.append(url_guess)
+                    except mechanize.HTTPError as e:
+                        pass
         return goodGuess
