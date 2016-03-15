406e6c6775f3bda8a9b0cf74b14a5c91e7fd6da5
Matt <mep2664@rit.edu>
2016-03-15 08:58:19 -0400

    Worked on sanitize. It is not 100% complete yet.

--- test/sanitize.py
+++ test/sanitize.py
@@ -2,3 +2,11 @@ class Sanitize(object):
 
 	def __init__(self, args):
 		print 'TODO'
+
+	def sanitized(self, inputs):
+		badData = ['<','>','"',"'"] #add more to this
+		for i in inputs:
+			for c in i:
+				if c in badData:
+					return False
+		return True
