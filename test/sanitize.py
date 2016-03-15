e33df6d1b5ef0cb2c52e08ccb8e42e5768570100
mep2664 <mep2664@rit.edu>
2016-03-15 16:51:12 -0400

    Updated sanitize.py

--- test/sanitize.py
+++ test/sanitize.py
@@ -5,48 +5,21 @@ class Sanitize(object):
         print 'TODO'
 
     def sanitized(self, inputs):
-        badData = ['<', '>', '"', ' ']  # add more to this
-        for i in inputs:
-            for c in i:
-                if c in badData:
-                    return False
-                if c == '&':
-                    self.isAndSanitized(i)
-        return True
-
-    def isAndSanitized(self, input):
-
+        badData = ['<', '>', '"', "'"]  # add more to this
         while '&' in input:
             pattern = re.compile('&\w+;')
             #matched = re.search(r'&\w+;', input)
             if pattern.search(input):
-                print(pattern.search(input).group())
+                if len(pattern.search(input).group()) < 4:
+                    return False
                 input = input[(input.find(pattern.search(input).group())+len(pattern.search(input).group())):]
             else:
                 return False
+        if any(substring in input for substring in badData):
+            return False
         return True
-
-
-        # escaped = ["&quot;", "&amp;", "&nbsp;", "&lt;", "&gt;"]
-        # tempInput = input
-        # while tempInput[0] != '&':
-        #     tempInput = tempInput[1:]
-        # count = tempInput.count('&')
-        # currCheck = 4   #the length of the shortest escaped character
-        # finCheck = 6    #the length of the longest escaped character
-        # while count > 0:
-        #     while currCheck <= finCheck:    # <= length of the longest escaped character
-        #         if len(tempInput) < 4:
-        #             return False
-        #         else:
-        #             if tempInput[0:currCheck+1] in escaped & count == 1:
-        #                 return True
-        #             elif tempInput[0:currCheck] in escaped & count > 1:
-        #                 tempInput = tempInput[currCheck:]
-        #     count -= 1
-
 
 if __name__ == '__main__':
     sanitizer = Sanitize()
-    input = "&&&"
-    sanitizer.isAndSanitized(input)
+    input = '&#88;'
+    print(sanitizer.sanitized(input))
